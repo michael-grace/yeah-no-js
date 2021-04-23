c05990c44b490309575a7b7293bf93e590c95658
Colin <9614541+ColinRoitt@users.noreply.github.com>
2021-04-23 22:58:27 +0100

    added dark mode

--- yeah-no.js
+++ yeah-no.js
@@ -22,7 +22,11 @@ var server = http.createServer(function(req, res) {
                     <!-- Michael Grace, August 2020, http://github.com/michael-grace -->
                     <h2> ${NAME} has said ${SAYING} </h2>
                     <h1>${data}</h1><h2> times. </h2>
-                    <script>setTimeout(function() {location.reload();}, 60000);</script></html>`);
+                    <script>setTimeout(function() {location.reload();}, 60000);
+                    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+                        document.querySelectorAll('*').forEach(el => {el.style.background = 'black'; el.style.color = 'white'})
+                    }
+                    </script></html>`);
 
             } else {
 
