feda6068f823948c1e3318d72fe6e2a3444dcc99
Colin <9614541+ColinRoitt@users.noreply.github.com>
2021-04-23 22:45:13 +0100

    Use template literals

--- yeah-no.js
+++ yeah-no.js
@@ -20,8 +20,8 @@ var server = http.createServer(function(req, res) {
                 res.write(`
                     <html style="text-align: center; font-family: Comic Sans MS, Arial; padding-top: 10em">
                     <!-- Michael Grace, August 2020, http://github.com/michael-grace -->
-                    <h2> ` + NAME + ` has said ` + SAYING + ` </h2>
-                    <h1>` + data + `</h1><h2> times. </h2>
+                    <h2> ${NAME} has said ${SAYING} </h2>
+                    <h1>${data}</h1><h2> times. </h2>
                     <script>setTimeout(function() {location.reload();}, 60000);</script></html>`);
 
             } else {
